442ff358c66d6524f3acd6e0b747bbb80be55625
Enriquez-maker <hugoe@leeschools.net>
2026-03-30 15:08:09 -0400

    Add image property to card objects

--- cards.js
+++ cards.js
@@ -1,11 +1,11 @@
 const CARDS = [
-    { id: "P1-01", name: "Mesala-Camala", rarity: "Common", page: 1 },
-    { id: "P1-02", name: "Lampadino-Girafino", rarity: "Common", page: 1 },
-    { id: "P1-03", name: "Tacorino-Tigrino", rarity: "Common", page: 1 },
-    { id: "P1-04", name: "Burger-Bufalino", rarity: "Uncommon", page: 1 },
-    { id: "P1-05", name: "Pianolino-Penguino", rarity: "Uncommon", page: 1 },
-    { id: "P1-06", name: "Spaghettino-Octopino", rarity: "Rare", page: 1 },
-    { id: "P1-07", name: "Gelatino-Rhino", rarity: "Rare", page: 1 },
-    { id: "P1-08", name: "Mozzarellino-Delfino", rarity: "Epic", page: 1 },
-    { id: "P1-09", name: "Grand Chef Camalestro", rarity: "Legendary", page: 1 }
+    { id: "P1-01", name: "Mesala-Camala", rarity: "Common", page: 1, image: "cards/P1-01.png" },
+    { id: "P1-02", name: "Lampadino-Girafino", rarity: "Common", page: 1, image: "cards/P1-02.png" },
+    { id: "P1-03", name: "Tacorino-Tigrino", rarity: "Common", page: 1, image: "cards/P1-03.png" },
+    { id: "P1-04", name: "Burger-Bufalino", rarity: "Uncommon", page: 1, image: "cards/P1-04.png" },
+    { id: "P1-05", name: "Pianolino-Penguino", rarity: "Uncommon", page: 1, image: "cards/P1-05.png" },
+    { id: "P1-06", name: "Spaghettino-Octopino", rarity: "Rare", page: 1, image: "cards/P1-06.png" },
+    { id: "P1-07", name: "Gelatino-Rhino", rarity: "Rare", page: 1, image: "cards/P1-07.png" },
+    { id: "P1-08", name: "Mozzarellino-Delfino", rarity: "Epic", page: 1, image: "cards/P1-08.png" },
+    { id: "P1-09", name: "Grand Chef Camalestro", rarity: "Legendary", page: 1, image: "cards/P1-09.png" }
 ];
