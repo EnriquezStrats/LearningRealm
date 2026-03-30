d1f50fa5d44613b3ef12ea8d0ace6acce2194f2e
Enriquez-maker <hugoe@leeschools.net>
2026-03-30 15:09:29 -0400

    Update card names in CARDS array

--- cards.js
+++ cards.js
@@ -5,7 +5,7 @@ const CARDS = [
     { id: "P1-04", name: "Burger-Bufalino", rarity: "Uncommon", page: 1, image: "cards/P1-04.png" },
     { id: "P1-05", name: "Pianolino-Penguino", rarity: "Uncommon", page: 1, image: "cards/P1-05.png" },
     { id: "P1-06", name: "Spaghettino-Octopino", rarity: "Rare", page: 1, image: "cards/P1-06.png" },
-    { id: "P1-07", name: "Gelatino-Rhino", rarity: "Rare", page: 1, image: "cards/P1-07.png" },
-    { id: "P1-08", name: "Mozzarellino-Delfino", rarity: "Epic", page: 1, image: "cards/P1-08.png" },
+    { id: "P1-07", name: "Gelatino-Rhinocerantino", rarity: "Rare", page: 1, image: "cards/P1-07.png" },
+    { id: "P1-08", name: "Mozzarcilino-Delfino", rarity: "Epic", page: 1, image: "cards/P1-08.png" },
     { id: "P1-09", name: "Grand Chef Camalestro", rarity: "Legendary", page: 1, image: "cards/P1-09.png" }
 ];
